172c227c6ff0c6694fae9c67622571ebc7adbe91
Subhankar Dey <140893109+SubhankarDey26@users.noreply.github.com>
2025-01-13 21:00:55 +0530

    Add files via upload

---
+++ Server/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import {registerUser, loginUser, userCredits, paymentRazorpay, verrifyRazorpay} from '../controllers/userController.js'
+import userAuth from '../middlewares/auth.js'
+
+const userRouter = express.Router()
+
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+userRouter.get('/credits', userAuth, userCredits)
+userRouter.post('/pay-razor', userAuth, paymentRazorpay)
+userRouter.post('/verify-razor',verrifyRazorpay)
+
+
+
+export default userRouter
